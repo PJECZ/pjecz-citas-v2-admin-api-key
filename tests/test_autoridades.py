89c3cbcd34bfae257ed72bfba717ccceb28c42a5
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2023-09-01 15:29:55 -0600

    Avance en unit tests

--- tests/test_autoridades.py
+++ tests/test_autoridades.py
@@ -1,42 +1,33 @@
 """
 Unit tests for autoridades category
 """
-import os
 import unittest
 
 import requests
-from dotenv import load_dotenv
 
-load_dotenv()
+from tests.load_env import config
 
 
 class TestAutoridades(unittest.TestCase):
     """Tests for autoridades category"""
 
-    def setUp(self) -> None:
-        """Initialize the test case"""
-        # Load environment variables
-        self.api_key = os.getenv("API_KEY", "")
-        self.host = os.getenv("HOST", "")
-        self.timeout = int(os.getenv("TIMEOUT", "20"))
-        # If any of the environment variables is empty, raise an error
-        if not self.api_key:
-            raise ValueError("API_KEY environment variable is empty")
-        if not self.host:
-            raise ValueError("HOST environment variable is empty")
-        if not self.timeout:
-            raise ValueError("TIMEOUT environment variable is empty")
-        # Return super
-        return super().setUp()
-
     def test_get_autoridades(self):
         """Test GET method for autoridades"""
-        response = requests.get(f"{self.host}/v3/autoridades", headers={"X-Api-Key": self.api_key}, timeout=self.timeout)
+        response = requests.get(
+            f"{config['host']}/v4/autoridades",
+            headers={"X-Api-Key": config["api_key"]},
+            timeout=config["timeout"],
+        )
         self.assertEqual(response.status_code, 200)
 
     def test_get_autoridades_by_es_jurisdiccional(self):
         """Test GET method for autoridades by es_jurisdiccional"""
-        response = requests.get(f"{self.host}/v3/autoridades", headers={"X-Api-Key": self.api_key}, params={"es_jurisdiccional": 1}, timeout=self.timeout)
+        response = requests.get(
+            f"{config['host']}/v4/autoridades",
+            headers={"X-Api-Key": config["api_key"]},
+            timeout=config["timeout"],
+            params={"es_jurisdiccional": 1},
+        )
         self.assertEqual(response.status_code, 200)
         data = response.json()
         self.assertEqual(data["success"], True)
@@ -45,7 +36,12 @@ class TestAutoridades(unittest.TestCase):
 
     def test_get_autoridades_by_es_organo_especializado(self):
         """Test GET method for autoridades by es_organo_especializado"""
-        response = requests.get(f"{self.host}/v3/autoridades", headers={"X-Api-Key": self.api_key}, params={"es_organo_especializado": 1}, timeout=self.timeout)
+        response = requests.get(
+            f"{config['host']}/v4/autoridades",
+            headers={"X-Api-Key": config["api_key"]},
+            timeout=config["timeout"],
+            params={"es_organo_especializado": 1},
+        )
         self.assertEqual(response.status_code, 200)
         data = response.json()
         self.assertEqual(data["success"], True)
---
+++ tests/test_usuario.py
@@ -0,0 +1,8 @@
+"""
+Unit tests for usuarios category
+"""
+import unittest
+
+import requests
+
+from tests.load_env import config
